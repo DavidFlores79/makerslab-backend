db7f4859ad48afb74ce517a2b2a4810787633c6f
David Flores <david.flores@itsoft.mx>
2025-06-03 23:20:14 -0600

    feat: add export to excel endpoint on summaries controller (admin)

--- controllers/summary.controller.js
+++ controllers/summary.controller.js
@@ -5,6 +5,7 @@ const summaryStatusModel = require('../models/summary_status.model')
 const userModel = require('../models/user.model')
 const configurationModel = require('../models/configuration.model');
 const { notifyUpdateSummary, notifyNewSummary } = require('../helpers/summary_notifications.helper');
+const ExcelJS = require('exceljs');
 
 getData = async (req, res) => {
     try {
@@ -196,7 +197,69 @@ deleteData = async (req, res) => {
     }
 }
 
+exportToExcel = async (req, res) => {
+  try {
+    // Query con filtros
+    const query = { deleted: false };
+
+    // Consulta para documentos
+    const summaries = await summaryModel.find(query)
+    .populate('document_status')
+    .populate({
+        path: 'owner',
+        select: ['name', 'email'],
+        populate: {
+        path: 'event_participant',
+        populate: [
+            { path: 'participation_mode', select: 'name' },
+            { path: 'owner', select: 'name' },
+            { path: 'creator', select: 'name' }
+        ]
+        }
+    })
+    .populate('creator', ['name', 'email']);
+
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Summaries');
+
+    worksheet.columns = [
+      { header: 'Grado Académico', key: 'academic_degree_name', width: 30 },
+      { header: 'Nombre', key: 'user_name', width: 30 },
+      { header: 'Institución', key: 'institution', width: 30 },
+      { header: 'Título', key: 'title', width: 150 },
+      { header: 'Correo', key: 'email', width: 50 },
+      { header: 'Fecha Carga', key: 'date', width: 20 },
+    ];
+
+    summaries.forEach((summary) => {
+      worksheet.addRow({
+        academic_degree_name: summary.owner ? summary.owner.event_participant.academic_degree_name : 'N/A',
+        user_name: summary.owner ? summary.owner.name : 'N/A',
+        institution: summary.owner ? summary.owner.event_participant.institution : 'N/A',
+        title: summary.title,
+        email: summary.owner ? summary.owner.email : 'N/A',
+        date: summary.createdAt.toISOString().split('T')[0],
+      });
+    });
+
+    res.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    );
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=summaries.xlsx'
+    );
+
+    await workbook.xlsx.write(res);
+    res.end();
+  } catch (error) {
+    console.error('Error al exportar a Excel:', error);
+    res.status(500).json({ error: 'Error al generar el archivo Excel.' });
+  }
+};
+
 //TODO: Obtener datos de la tabla thematic_areas o topics
 
 
-module.exports = { getData, postData, updateData, deleteData, getPaymentMethods }
+module.exports = { getData, postData, updateData, deleteData, getPaymentMethods, exportToExcel }
--- routes/summaries.routes.js
+++ routes/summaries.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator')
-const { getData, postData, updateData, deleteData } = require('../controllers/summary.controller');
+const { getData, postData, updateData, deleteData, exportToExcel } = require('../controllers/summary.controller');
 const { validateSummaryById } = require('../helpers/db_validators.helper');
 const { checkRoleAuth } = require('../middlewares/role-validator.middleware');
 const { validarJWT } = require('../middlewares/validar-jwt.middleware');
@@ -33,5 +33,6 @@ router.delete('/:id', [
     check('id').custom( validateSummaryById ),
     Validator
 ], deleteData);
+router.get('/export', exportToExcel);
 
 module.exports = router
